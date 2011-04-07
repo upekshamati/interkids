caec31dc6a29b95830c4b510f452fd1e50431d2b
Israel Buitron <israel.buitron@gmail.com>
2011-04-07 04:41:00 -0500

    Rails gem update is required. This update is mandatory because in this version were fixed some issues.
    Please run:
    
    gem install -v=2.3.11 rails
    
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 require File.join(File.dirname(__FILE__), 'boot')
 
 
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
@@ -10,8 +10,8 @@ Rails::Initializer.run do |config|
   config.gem 'searchlogic'
   config.gem 'prawn', :version=> '0.6.3'
 
-  config.load_once_paths += %W( #{RAILS_ROOT}/lib )
-  config.load_paths += Dir["#{RAILS_ROOT}/app/models/*"].find_all { |f| File.stat(f).directory? }
+  config.autoload_once_paths += %W( #{RAILS_ROOT}/lib )
+  config.autoload_paths += Dir["#{RAILS_ROOT}/app/models/*"].find_all { |f| File.stat(f).directory? }
   
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
